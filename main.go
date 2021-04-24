0487ee86555244c5740b9992943a3286d7eef7f5
dima <hexdump1337@gmail.com>
2021-04-24 23:46:30 +0300

    resources added

--- main.go
+++ main.go
@@ -15,11 +15,11 @@ func main() {
         fmt.Print("> ")
         input, err := reader.ReadString('\n')
         if err != nil {
-           fmt.Fprintln(os.Stderr, err)
+            fmt.Fprintln(os.Stderr, err)
         }
-        
+
         if err = execInput(input); err != nil {
-           fmt.Fprintln(os.Stderr, err)
+            fmt.Fprintln(os.Stderr, err)
         }
     }
 }
@@ -28,23 +28,23 @@ var ErrNoPath = errors.New("Path is required!")
 
 func execInput(input string) error {
     input = strings.TrimSuffix(input, "\n")
-    
+
     args := strings.Split(input, " ")
-    
+
     switch args[0] {
     case "cd":
         if len(args) < 2 {
-           return ErrNoPath
+            return ErrNoPath
         }
         return os.Chdir(args[1])
     case "exit":
         os.Exit(0)
     }
-    
+
     cmd := exec.Command(args[0], args[1:]...)
-    
+
     cmd.Stderr = os.Stderr
     cmd.Stdout = os.Stdout
-    
+
     return cmd.Run()
 }
